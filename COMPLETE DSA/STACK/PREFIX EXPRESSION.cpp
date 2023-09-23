b430eeaf2dedf4fac89045c98dedf1735b4f81cd
Arya1133 <105840756+Arya1133@users.noreply.github.com>
2023-09-23 14:13:51 +0530

    Update PREFIX EXPRESSION.cpp

--- COMPLETE DSA/STACK/PREFIX EXPRESSION.cpp
+++ COMPLETE DSA/STACK/PREFIX EXPRESSION.cpp
@@ -1,8 +1,42 @@
 #include<iostream>
 #include<stack>
 #include<math.h>
+
 using namespace std;
-int prefixevaluation(string s){
-    stack <int > st;
-    for(int i=s.length()-1;i>=0;i--)
+int prefix(string s){
+    stack<int> st;
+    for(int i=s.length()-1;i>=0;i--){
+        if(s[i]>='0' && s[i]<='9'){
+            st.push(s[i] -'0');
+        }
+        else{
+            int op1=st.top();
+            st.pop();
+            int op2=st.top();
+            st.pop();
+            switch([i]){
+                case '+':
+                st.push(op1+op2);
+                break;
+                case '-':
+                st.push(op1-op2);
+                break;
+                case '*':
+                st.push(op1*op2);
+                break;
+                case '/':
+                st.push(op1/op2);
+                break;
+                case '^':
+                st.push(op1,op2);
+                break;
+                default:
+                break;            }}}
+                returnn st.top();
+
+    
+
+int main(){
+    cout<<prefix("-+7*45+20")<<endl;
+    return 0;
 }
